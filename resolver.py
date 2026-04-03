d7a33bc8bc32624f2ef7c6d605fd7d51ed6f7beb
Prestondevs <Prestonvardaman1@gmail.com>
2026-04-02 22:37:11 -0500

    fixed input kw used, chars bitvector, and solver restraints

--- resolver.py
+++ resolver.py
@@ -1,11 +1,17 @@
 import z3
 
-#1 char = 8 bits, 16 chars = 128
-input = z3.BitVec('char', 128)
-solver = z3.Solver()
+def analyze_gates(input_gates: int):
 
-#printable ascii char restraints " " & ""
-solver.add(char > 0x20)
-solver.add(char < 0x7E)
+    # 1 char = 8 bits, 16 chars = 128
+    chars = z3.BitVec('chars', 128)
+    solver = z3.Solver()
+
+    # printable ascii char restraints " " - "~"
+    solver.add(chars > 0x20)
+    solver.add(chars < 0x7E)
+
+    return solver.model()
 
 
+if __name__ == "__main__":
+    analyze_gates(16)
